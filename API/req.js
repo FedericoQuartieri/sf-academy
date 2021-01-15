57e1c57c5b5e033fc8c355071a4a0f2d8d3ee7a2
Federico Quartieri <federico.quartieri2002@gmail.com>
2021-01-15 15:40:52 +0100

    deleted files

--- API/req.js
+++
@@ -1,5 +0,0 @@
-
-var request = require('sync-request');
-filmname = "die-hard"
-search = JSON.parse(request('GET', `http://www.omdbapi.com/?t=${filmname}&apikey=b5c264d3`).getBody().toString())
-console.log(search)
